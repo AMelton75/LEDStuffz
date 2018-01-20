853534552cc9bc5d6aca38c4f64462fde5531a61
AMelton75 <35177730+AMelton75@users.noreply.github.com>
2018-01-20 14:27:23 -0800

    Add files via upload

--- led_tests.ino
+++ led_tests.ino
@@ -13,14 +13,20 @@ int ri = 1;
 int gi;
 int bi;
 
+int light = 25;
+int lightd = 1;
+
 int LSDBreath = 0;
 bool LSDup = true;
 
-int state = 2;
+int snake = 0;
+int state = 1;
 Adafruit_DotStar strip = Adafruit_DotStar(STRIP_LENGTH, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
+int current = millis();
+int reset = millis();
 void setup() {
-  // put your setup code here, to run once:
+  //put your setup code here, to run once:
   strip.begin();
   strip.show();
   Serial.begin(9600);
@@ -28,15 +34,43 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  current = millis();
+  if (current - reset >= 30000) {
+    state++;
+    if (state >= 4) {
+      state = 0;
+    }
+    reset = millis();
+    r = 0;
+    g = 0;
+    b = 0;
+
+    ri = 1;
+    gi = 0;
+    bi = 0;
+
+    light = 25;
+    lightd = 1;
+
+    LSDBreath = 0;
+    LSDup = true;
+
+    snake = 0;
+  }
   if (state == 0) {
     wierdRainbow();
   }
   if (state == 1) {
-    rainbow();
+    rainbowSnake();
   }
   if (state == 2) {
     LSD();
   }
+  if (state == 3) {
+    rainbowCool();
+  }
+
+
   Serial.println(r);
   Serial.println(g);
   Serial.println(b);
@@ -73,7 +107,7 @@ void wierdRainbow() {
   b += bi;
 }
 
-void rainbow() {
+void rainbowSnake() {
   for (int i = 0; i < STRIP_LENGTH / 3; i++) {
     strip.setPixelColor((i * 3), r, g, b);
   }
@@ -82,6 +116,13 @@ void rainbow() {
   }
   for (int j = 0; j < STRIP_LENGTH / 3; j++) {
     strip.setPixelColor((j * 3 + 2), r, g, b);
+  }
+  for (int i = 0; i < 50; i++) {
+    strip.setPixelColor((snake - i), i + 5, i + 5, i + 5);
+  }
+  snake++;
+  if (snake - 50 >= STRIP_LENGTH) {
+    snake = 0;
   }
   strip.show();
   if (r >= 50) {
@@ -112,22 +153,53 @@ void LSD() {
     g = random(0, LSDBreath);
     b = random(0, LSDBreath);
   }
-  if (LSDup == true && LSDBreath <= 100) {
+  if (LSDup == true && LSDBreath <= 50) {
     LSDBreath++;
   }
-  else if(LSDup == true){
+  else if (LSDup == true) {
     LSDup = false;
   }
   if (LSDup == false && LSDBreath >= 10) {
     LSDBreath--;
   }
-  else if(LSDup == false){
+  else if (LSDup == false) {
     LSDup = true;
   }
   strip.show();
 }
 
-
+void rainbowCool() {
+  for (int i = 0; i < STRIP_LENGTH; i++) {
+    strip.setPixelColor((i), r * 25 / light, g * 25 / light, b * 25 / light);
+  }
+  strip.show();
+  if (r >= 50) {
+    ri = -1;
+    gi = 1;
+    bi = 0;
+  }
+  if (g >= 50) {
+    gi = -1;
+    bi = 1;
+    ri = 0;
+  }
+  if (b >= 50) {
+    bi = -1;
+    ri = 1;
+    gi = 0;
+  }
+  if (light >= 50 ) {
+    lightd = -1;
+  }
+  if (light <= 25 ) {
+    lightd = 1;
+  }
+  r += ri;
+  g += gi;
+  b += bi;
+  light += lightd;
+  delay(20);
+}
 
 
 
